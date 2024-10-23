d506cbd5e62e3c4f99e54d6b9bdf8ddacd2414ba
red5191 <the_red51@outlook.com>
2024-10-24 00:32:03 +0300

    1323

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def countdown():
-    try:
-        x = int(input('Введи целое, положительное число '))
-        while True:
+    while True:
+        try:
+            x = int(input('Введи целое, положительное число '))
             if x > 0:
                 break
             else:
                 print('Это не положительное', '\n')
-                x = int(input('Введи целое, положительное число '))
-    except ValueError:
-        print('Это не число', '\n')
+                # x = int(input('Введи целое, положительное число '))
+        except ValueError:(
+            print('Это не число', '\n'))
     while x >= 0:
         print(x)
         x -= 1
